05bc89191942127f5df4ed402e7e834961e28173
“Елена <borovina@bk.ru>
2022-04-03 21:39:21 +0300

    добвлен импорт

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -8,6 +8,7 @@ from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
+from .permissions import AuthorOrReadOnly, IsAdminOrSuperUser, IsAdminPermission, ReadOnlyPermission
 
 from reviews.models import Category, Genre, Review, Title
 from users.models import User
