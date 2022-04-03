066d0278e620f524887a2bb91b0a829888a0538c
“Елена <borovina@bk.ru>
2022-04-03 20:42:04 +0300

    добавила премишен

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -25,3 +25,18 @@ class IsAdminOrDjangoAdminOrReadOnly(permissions.BasePermission):
                 'POST', 'DELETE', 'PATCH') and request.user.is_authenticated:
             return request.user.is_admin
         return request.method == 'GET'
+
+
+class IsAdminPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return (request.user and request.user.is_authenticated
+                and request.user.is_admin)
+
+    def has_object_permission(self, request, view, obj):
+        return (request.user and request.user.is_authenticated
+                and request.user.is_admin)
+
+
+class ReadOnlyPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.method in permissions.SAFE_METHODS
