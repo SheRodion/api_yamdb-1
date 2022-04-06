e87982176c98ff5d17439f897f3a3d7609dc1156
Vyacheslav Borisenko <borisenkov89@yandex.ru>
2022-04-06 17:06:26 +0300

    refactoring api/urls

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -6,14 +6,20 @@ from .views import (CategoryViewSet, CommentViewSet, GenreViewSet,
                     get_confirmation_code, get_jwt_token)
 
 router = routers.DefaultRouter()
-router.register(r'categories', CategoryViewSet, basename='categories')
-router.register(r'genres', GenreViewSet, basename='genres')
-router.register(r'titles', TitleViewSet, basename='titles')
+router.register('categories', CategoryViewSet, basename='categories')
+router.register('genres', GenreViewSet, basename='genres')
+router.register('titles', TitleViewSet, basename='titles')
 router.register('users', UserViewSet)
-router.register(r'titles/(?P<title_id>\d+)/reviews',
-                ReviewViewSet, basename='reviews')
-router.register(r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)'
-                r'/comments', CommentViewSet, basename='comments')
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet,
+    basename='reviews'
+)
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
 
 urlpatterns = [
     path('v1/', include(router.urls)),
